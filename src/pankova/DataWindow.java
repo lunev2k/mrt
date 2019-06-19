d0eb723e9af28f1c5ea24ebcece30cb95e4efc7a
lunev2k <lunev2k@gmail.com>
2019-06-19 12:35:53 +0700

    Adding calculate

--- src/pankova/DataWindow.java
+++ src/pankova/DataWindow.java
@@ -6,14 +6,11 @@ import java.awt.event.ActionListener;
 
 public class DataWindow extends JFrame {
     private JPanel panel;
-    private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
-    private JTextField textField6;
     private JButton resultButton;
-    private JLabel label1;
     private JLabel label2;
     private JLabel label3;
     private JLabel label4;
@@ -34,6 +31,39 @@ public class DataWindow extends JFrame {
 
     private class ResultActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            double d = Float.parseFloat(textField2.getText());
+            double d1 = Float.parseFloat(textField3.getText());
+            double pg1 = Float.parseFloat(textField4.getText());
+            double pg2 = Float.parseFloat(textField5.getText());
+            double ak = 0.785 * (Math.pow(d, 2) - Math.pow(d1, 2));
+            double ad = Math.pow(d, 2) * 0.785;
+            double sp = 100 * ak / ad;
+            double pk = ak / ad;
+            double hnu = 10 / pk;
+            double hny = 10 / d1;
+            String text = "";
+
+            if (pg1 < -15) {
+                text = "Межпозвоночная грыжа приблизительно равна 0.6 см";
+            }
+            if (pg1 < -30) {
+                text = "Межпозвоночная грыжа приблизительно равна 1 см";
+            }
+            if (pg1 < -90) {
+                text = "Межпозвоночный диск не существует";
+            }
+
+            if (pg2 < -15) {
+                text = "Межпозвоночная грыжа приблизительно равна 0.6 см";
+            }
+            if (pg2 < -30) {
+                text = "Межпозвоночная грыжа приблизительно равна 1 см";
+            }
+            if (pg2 < -90) {
+                text = "Межпозвоночный диск не существует";
+            }
+
+            resultWindow.setText(text);
             resultWindow.setVisible(true);
         }
     }
--- src/pankova/ResultWindow.java
+++ src/pankova/ResultWindow.java
@@ -7,9 +7,20 @@ public class ResultWindow extends JFrame {
     private JPanel panel;
     private JTextArea textArea1;
 
+    private String text;
+
     public ResultWindow() {
         super("Результат обследования");
         getContentPane().add(panel);
         setLocationRelativeTo(null);
+        textArea1.setText(getText());
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
